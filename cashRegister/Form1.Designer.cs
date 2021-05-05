dfb893dc3c676ca22cb8c37a98e82cb59a1fadca
Hewan Amare <hewaamar557@ed.amdsb.ca>
2021-05-05 10:15:12 -0400

    upload #2

--- cashRegister/Form1.Designer.cs
+++ cashRegister/Form1.Designer.cs
@@ -29,10 +29,11 @@ namespace cashRegister
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.label1 = new System.Windows.Forms.Label();
-            this.item1Label = new System.Windows.Forms.Label();
-            this.item2Label = new System.Windows.Forms.Label();
-            this.item3Label = new System.Windows.Forms.Label();
+            this.pencilLabel = new System.Windows.Forms.Label();
+            this.staplerLabel = new System.Windows.Forms.Label();
+            this.caseLabel = new System.Windows.Forms.Label();
             this.pencilInput = new System.Windows.Forms.TextBox();
             this.caseInput = new System.Windows.Forms.TextBox();
             this.staplerInput = new System.Windows.Forms.TextBox();
@@ -51,204 +52,175 @@ namespace cashRegister
             this.changeOutput = new System.Windows.Forms.Label();
             this.printButton = new System.Windows.Forms.Button();
             this.receiptOutput = new System.Windows.Forms.Label();
+            this.newButton = new System.Windows.Forms.Button();
+            this.totalErrorOutput = new System.Windows.Forms.Label();
+            this.changeAmountOutput = new System.Windows.Forms.Label();
+            this.changeLetterOutput = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(145, 22);
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.ForeColor = System.Drawing.Color.LightCoral;
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(110, 20);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Cash Register";
             // 
-            // item1Label
+            // pencilLabel
             // 
-            this.item1Label.AutoSize = true;
-            this.item1Label.Location = new System.Drawing.Point(56, 72);
-            this.item1Label.Name = "item1Label";
-            this.item1Label.Size = new System.Drawing.Size(54, 20);
-            this.item1Label.TabIndex = 1;
-            this.item1Label.Text = "Item 1";
+            resources.ApplyResources(this.pencilLabel, "pencilLabel");
+            this.pencilLabel.Name = "pencilLabel";
             // 
-            // item2Label
+            // staplerLabel
             // 
-            this.item2Label.AutoSize = true;
-            this.item2Label.Location = new System.Drawing.Point(56, 135);
-            this.item2Label.Name = "item2Label";
-            this.item2Label.Size = new System.Drawing.Size(54, 20);
-            this.item2Label.TabIndex = 2;
-            this.item2Label.Text = "Item 2";
+            resources.ApplyResources(this.staplerLabel, "staplerLabel");
+            this.staplerLabel.Name = "staplerLabel";
             // 
-            // item3Label
+            // caseLabel
             // 
-            this.item3Label.AutoSize = true;
-            this.item3Label.Location = new System.Drawing.Point(56, 195);
-            this.item3Label.Name = "item3Label";
-            this.item3Label.Size = new System.Drawing.Size(54, 20);
-            this.item3Label.TabIndex = 3;
-            this.item3Label.Text = "Item 3";
-            this.item3Label.Click += new System.EventHandler(this.item3Label_Click);
+            resources.ApplyResources(this.caseLabel, "caseLabel");
+            this.caseLabel.Name = "caseLabel";
             // 
             // pencilInput
             // 
-            this.pencilInput.Location = new System.Drawing.Point(149, 72);
+            this.pencilInput.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            resources.ApplyResources(this.pencilInput, "pencilInput");
             this.pencilInput.Name = "pencilInput";
-            this.pencilInput.Size = new System.Drawing.Size(195, 26);
-            this.pencilInput.TabIndex = 4;
             // 
             // caseInput
             // 
-            this.caseInput.Location = new System.Drawing.Point(149, 195);
+            resources.ApplyResources(this.caseInput, "caseInput");
             this.caseInput.Name = "caseInput";
-            this.caseInput.Size = new System.Drawing.Size(195, 26);
-            this.caseInput.TabIndex = 5;
             // 
             // staplerInput
             // 
-            this.staplerInput.Location = new System.Drawing.Point(149, 129);
+            resources.ApplyResources(this.staplerInput, "staplerInput");
             this.staplerInput.Name = "staplerInput";
-            this.staplerInput.Size = new System.Drawing.Size(195, 26);
-            this.staplerInput.TabIndex = 6;
             // 
             // totalButton
             // 
-            this.totalButton.Location = new System.Drawing.Point(121, 242);
+            this.totalButton.BackColor = System.Drawing.Color.LightCyan;
+            resources.ApplyResources(this.totalButton, "totalButton");
             this.totalButton.Name = "totalButton";
-            this.totalButton.Size = new System.Drawing.Size(181, 41);
-            this.totalButton.TabIndex = 7;
-            this.totalButton.Text = "Calculate Total";
-            this.totalButton.UseVisualStyleBackColor = true;
+            this.totalButton.UseVisualStyleBackColor = false;
             this.totalButton.Click += new System.EventHandler(this.totalButton_Click);
             // 
             // label2
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(45, 472);
+            resources.ApplyResources(this.label2, "label2");
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(0, 20);
-            this.label2.TabIndex = 8;
             // 
             // subtotalLabel
             // 
-            this.subtotalLabel.AutoSize = true;
-            this.subtotalLabel.Location = new System.Drawing.Point(86, 304);
+            resources.ApplyResources(this.subtotalLabel, "subtotalLabel");
             this.subtotalLabel.Name = "subtotalLabel";
-            this.subtotalLabel.Size = new System.Drawing.Size(77, 20);
-            this.subtotalLabel.TabIndex = 9;
-            this.subtotalLabel.Text = "Sub Total";
             // 
             // taxLabel
             // 
-            this.taxLabel.AutoSize = true;
-            this.taxLabel.Location = new System.Drawing.Point(86, 363);
+            resources.ApplyResources(this.taxLabel, "taxLabel");
             this.taxLabel.Name = "taxLabel";
-            this.taxLabel.Size = new System.Drawing.Size(34, 20);
-            this.taxLabel.TabIndex = 10;
-            this.taxLabel.Text = "Tax";
-            this.taxLabel.Click += new System.EventHandler(this.label3_Click);
+            this.taxLabel.Click += new System.EventHandler(this.totalButton_Click);
             // 
             // totalLabel
             // 
-            this.totalLabel.AutoSize = true;
-            this.totalLabel.Location = new System.Drawing.Point(86, 416);
+            resources.ApplyResources(this.totalLabel, "totalLabel");
             this.totalLabel.Name = "totalLabel";
-            this.totalLabel.Size = new System.Drawing.Size(44, 20);
-            this.totalLabel.TabIndex = 11;
-            this.totalLabel.Text = "Total";
             // 
             // tenderedLabel
             // 
-            this.tenderedLabel.AutoSize = true;
-            this.tenderedLabel.Location = new System.Drawing.Point(86, 477);
+            resources.ApplyResources(this.tenderedLabel, "tenderedLabel");
             this.tenderedLabel.Name = "tenderedLabel";
-            this.tenderedLabel.Size = new System.Drawing.Size(77, 20);
-            this.tenderedLabel.TabIndex = 12;
-            this.tenderedLabel.Text = "Tendered";
             // 
             // taxOutput
             // 
             this.taxOutput.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.taxOutput.Location = new System.Drawing.Point(267, 363);
+            resources.ApplyResources(this.taxOutput, "taxOutput");
             this.taxOutput.Name = "taxOutput";
-            this.taxOutput.Size = new System.Drawing.Size(77, 33);
-            this.taxOutput.TabIndex = 14;
             // 
             // totalOutput
             // 
             this.totalOutput.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.totalOutput.Location = new System.Drawing.Point(267, 417);
+            resources.ApplyResources(this.totalOutput, "totalOutput");
             this.totalOutput.Name = "totalOutput";
-            this.totalOutput.Size = new System.Drawing.Size(77, 33);
-            this.totalOutput.TabIndex = 15;
             // 
             // tenderedInput
             // 
-            this.tenderedInput.Location = new System.Drawing.Point(271, 471);
+            resources.ApplyResources(this.tenderedInput, "tenderedInput");
             this.tenderedInput.Name = "tenderedInput";
-            this.tenderedInput.Size = new System.Drawing.Size(73, 26);
-            this.tenderedInput.TabIndex = 16;
             // 
             // changeButton
             // 
-            this.changeButton.Location = new System.Drawing.Point(146, 520);
+            this.changeButton.BackColor = System.Drawing.Color.LightCyan;
+            resources.ApplyResources(this.changeButton, "changeButton");
             this.changeButton.Name = "changeButton";
-            this.changeButton.Size = new System.Drawing.Size(166, 47);
-            this.changeButton.TabIndex = 17;
-            this.changeButton.Text = "Calculate Change";
-            this.changeButton.UseVisualStyleBackColor = true;
+            this.changeButton.UseVisualStyleBackColor = false;
             this.changeButton.Click += new System.EventHandler(this.changeButton_Click);
             // 
             // subtotalOutput
             // 
             this.subtotalOutput.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.subtotalOutput.Location = new System.Drawing.Point(267, 304);
+            resources.ApplyResources(this.subtotalOutput, "subtotalOutput");
             this.subtotalOutput.Name = "subtotalOutput";
-            this.subtotalOutput.Size = new System.Drawing.Size(77, 33);
-            this.subtotalOutput.TabIndex = 18;
             // 
             // changeLabel
             // 
-            this.changeLabel.AutoSize = true;
-            this.changeLabel.Location = new System.Drawing.Point(100, 610);
+            resources.ApplyResources(this.changeLabel, "changeLabel");
             this.changeLabel.Name = "changeLabel";
-            this.changeLabel.Size = new System.Drawing.Size(65, 20);
-            this.changeLabel.TabIndex = 19;
-            this.changeLabel.Text = "Change";
             // 
             // changeOutput
             // 
             this.changeOutput.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.changeOutput.Location = new System.Drawing.Point(267, 610);
+            resources.ApplyResources(this.changeOutput, "changeOutput");
             this.changeOutput.Name = "changeOutput";
-            this.changeOutput.Size = new System.Drawing.Size(77, 33);
-            this.changeOutput.TabIndex = 20;
             // 
             // printButton
             // 
-            this.printButton.Location = new System.Drawing.Point(146, 674);
+            this.printButton.BackColor = System.Drawing.Color.LightCyan;
+            resources.ApplyResources(this.printButton, "printButton");
             this.printButton.Name = "printButton";
-            this.printButton.Size = new System.Drawing.Size(163, 39);
-            this.printButton.TabIndex = 21;
-            this.printButton.Text = "Print Receipt";
-            this.printButton.UseVisualStyleBackColor = true;
+            this.printButton.UseVisualStyleBackColor = false;
             this.printButton.Click += new System.EventHandler(this.printButton_Click);
             // 
             // receiptOutput
             // 
             this.receiptOutput.BackColor = System.Drawing.SystemColors.ControlLightLight;
-            this.receiptOutput.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.receiptOutput.Location = new System.Drawing.Point(400, 42);
+            resources.ApplyResources(this.receiptOutput, "receiptOutput");
             this.receiptOutput.Name = "receiptOutput";
-            this.receiptOutput.Size = new System.Drawing.Size(396, 479);
-            this.receiptOutput.TabIndex = 22;
-            this.receiptOutput.Click += new System.EventHandler(this.receipt_Click);
+            // 
+            // newButton
+            // 
+            this.newButton.BackColor = System.Drawing.Color.PaleGoldenrod;
+            resources.ApplyResources(this.newButton, "newButton");
+            this.newButton.Name = "newButton";
+            this.newButton.UseVisualStyleBackColor = false;
+            this.newButton.Click += new System.EventHandler(this.newButton_Click);
+            // 
+            // totalErrorOutput
+            // 
+            resources.ApplyResources(this.totalErrorOutput, "totalErrorOutput");
+            this.totalErrorOutput.ForeColor = System.Drawing.Color.IndianRed;
+            this.totalErrorOutput.Name = "totalErrorOutput";
+            // 
+            // changeAmountOutput
+            // 
+            resources.ApplyResources(this.changeAmountOutput, "changeAmountOutput");
+            this.changeAmountOutput.ForeColor = System.Drawing.Color.IndianRed;
+            this.changeAmountOutput.Name = "changeAmountOutput";
+            // 
+            // changeLetterOutput
+            // 
+            resources.ApplyResources(this.changeLetterOutput, "changeLetterOutput");
+            this.changeLetterOutput.ForeColor = System.Drawing.Color.IndianRed;
+            this.changeLetterOutput.Name = "changeLetterOutput";
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            resources.ApplyResources(this, "$this");
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(826, 763);
+            this.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.Controls.Add(this.changeLetterOutput);
+            this.Controls.Add(this.changeAmountOutput);
+            this.Controls.Add(this.totalErrorOutput);
+            this.Controls.Add(this.newButton);
             this.Controls.Add(this.receiptOutput);
             this.Controls.Add(this.printButton);
             this.Controls.Add(this.changeOutput);
@@ -267,12 +239,12 @@ namespace cashRegister
             this.Controls.Add(this.staplerInput);
             this.Controls.Add(this.caseInput);
             this.Controls.Add(this.pencilInput);
-            this.Controls.Add(this.item3Label);
-            this.Controls.Add(this.item2Label);
-            this.Controls.Add(this.item1Label);
+            this.Controls.Add(this.caseLabel);
+            this.Controls.Add(this.staplerLabel);
+            this.Controls.Add(this.pencilLabel);
             this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Name = "Form1";
-            this.Text = "Form1";
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -281,9 +253,9 @@ namespace cashRegister
         #endregion
 
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label item1Label;
-        private System.Windows.Forms.Label item2Label;
-        private System.Windows.Forms.Label item3Label;
+        private System.Windows.Forms.Label pencilLabel;
+        private System.Windows.Forms.Label staplerLabel;
+        private System.Windows.Forms.Label caseLabel;
         private System.Windows.Forms.TextBox pencilInput;
         private System.Windows.Forms.TextBox caseInput;
         private System.Windows.Forms.TextBox staplerInput;
@@ -302,6 +274,10 @@ namespace cashRegister
         private System.Windows.Forms.Label changeOutput;
         private System.Windows.Forms.Button printButton;
         private System.Windows.Forms.Label receiptOutput;
+        private System.Windows.Forms.Button newButton;
+        private System.Windows.Forms.Label totalErrorOutput;
+        private System.Windows.Forms.Label changeAmountOutput;
+        private System.Windows.Forms.Label changeLetterOutput;
     }
 }
 
--- cashRegister/Form1.cs
+++ cashRegister/Form1.cs
@@ -7,115 +7,224 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using System.Threading; // allows Thread.Sleep()
+using System.Media;  // allows sound effects
+using System.Threading; // allows Thread.Sleep() (pauses between printing receipt)
+
+        // HEWAN AMARE
+        // ICS3U, MR.T
+        // CASH REGISTER SUMMATIVE
 
 namespace cashRegister
 {
     public partial class Form1 : Form
     {
+        // state item prices
         double pencilPrice = 5.00;
         double staplerPrice = 10.00;
         double casePrice = 15.00;
 
+        // state item quantities
         double pencilQuantity = 0;
         double staplerQuantity = 0;
         double caseQuantity = 0;
 
+        // state transaction details
         double subtotal = 0;
         double taxRate = 0.13;
         double taxAmount = 0;
         double total;
-        double tendered;
+        double tendered = 0;
         double change;
 
+        // create a pause time for receipt printing
+        int pause = 600;
 
         public Form1()
         {
             InitializeComponent();
+
+            // disable all buttons except total button
+            changeButton.Enabled = false;
+            printButton.Enabled = false;
+            newButton.Enabled = false;
         }
 
-        private void label3_Click(object sender, EventArgs e)
+        private void totalButton_Click(object sender, EventArgs e) // CLICK TOTAL BUTTON
         {
+            try {
 
+                // Extend length of form
+                Form1.ActiveForm.Height = 600;
+
+                // change pencil input to numbers only
+                pencilQuantity = Convert.ToDouble(pencilInput.Text);
+
+                // change stapler input to numbers only
+                staplerQuantity = Convert.ToDouble(staplerInput.Text);
+
+                // change pencil case input to numbers only
+                caseQuantity = Convert.ToDouble(caseInput.Text);
+
+                // calculate subtotal
+                subtotal = pencilQuantity * pencilPrice + staplerQuantity * staplerPrice + caseQuantity * casePrice;
+                // display subtotal
+                subtotalOutput.Text = $" {subtotal.ToString("C")}";
+
+                // calculate tax
+                taxAmount = taxRate * subtotal;
+                // display tax alone on purchase
+                taxOutput.Text = $"{taxAmount.ToString("C")}";
+
+                // calculate total cost (with tax)
+                total = subtotal + taxAmount;
+
+                // display total cost
+                totalOutput.Text = $"{total.ToString("C")}";
+
+                // enable change button
+                changeButton.Enabled = true;
+
+                // make the error message invisible
+                totalErrorOutput.Visible = false;
+
+            }
+
+            catch {
+                totalErrorOutput.Text = "Please input numerical values";
+            }
         }
 
-        private void totalButton_Click(object sender, EventArgs e) // TOTAL BUTTON
+        private void changeButton_Click(object sender, EventArgs e) // CLICK CHANGE BUTTON
         {
-            // disable 'Calculate Change' button
+            try
+            {
+                // change tendered input to numbers only
+                tendered = Convert.ToDouble(tenderedInput.Text);
 
+                // state change value
+                change = tendered - total;
 
-            // change pencil input to numbers only
-            pencilQuantity = Convert.ToDouble(pencilInput.Text);
-            // change stapler input to numbers only
-            staplerQuantity = Convert.ToDouble(staplerInput.Text);
-            // change pencil case input to numbers only
-            caseQuantity = Convert.ToDouble(caseInput.Text);
+                // display change
+                changeOutput.Text = $"{change.ToString("C")}";
 
-            // calculate subtotal
-            subtotal = pencilQuantity * pencilPrice + staplerQuantity * staplerPrice + caseQuantity * casePrice;
-            // display subtotal
-            subtotalOutput.Text = $" {subtotal.ToString("C")}";
+                //enable print button
+                printButton.Enabled = true;
 
-            // calculate tax
-            taxAmount = taxRate * subtotal;
-            // display tax alone on purchase
-            taxOutput.Text = $"{taxAmount.ToString("C")}";
+                // make the error message for money amount invisible
+                changeAmountOutput.Visible = false;
 
-            // calculate total cost (with tax)
-            total = subtotal + taxAmount;
-            // display total cost
-            totalOutput.Text = $"{total.ToString("C")}";
+                // make the error message for wrong characters invisible
+                changeLetterOutput.Visible = false;
 
+                if (tendered < total)
+                {
+                    // tell user to enter enough money to pay
+                    changeAmountOutput.Text += ("Please input a sufficient amount.");
+
+                    // make message visible
+                    changeAmountOutput.Visible = true;
+                    changeLetterOutput.Visible = false;
+
+                    // disable print button to prevent customer from not paying enough money
+                    printButton.Enabled = false;
+                }
+
+                else
+                {
+                    
+                }
+            }
+
+            catch
+            {
+                // display error message when numbers are not entered
+                changeLetterOutput.Text = "Please enter a numerical value.";
+
+                // make error message for wrong character submission visible
+                changeLetterOutput.Visible = true;
+
+                // make error message for insufficient tendered amount invisible
+                changeAmountOutput.Visible = false;
+
+            }
         }
 
-        private void changeButton_Click(object sender, EventArgs e) // CHANGE BUTTON
-        {
-            // change tendered input to numbers only
-            tendered = Convert.ToDouble(tenderedInput.Text);
-            // state change value
-            change = tendered - total;
-            // display change
-            changeOutput.Text = $"{change.ToString("C")}";
-        }
 
-        private void receipt_Click(object sender, EventArgs e)
+        private void printButton_Click(object sender, EventArgs e) // CLICK PRINT BUTTON
         {
 
-        }
+            // extend width to show receipt
+            Form1.ActiveForm.Width = 700;
 
-        private void printButton_Click(object sender, EventArgs e)
-        {
- 
+            SoundPlayer player = new SoundPlayer(Properties.Resources.printSound);
+            player.Play();
 
-
+            // enable new order button
+            newButton.Enabled = true;
 
             // display receipt title
             receiptOutput.Text = "        Your Receipt";
+
             // display receipt headers and pause
             receiptOutput.Text += "\n\n Item           Quantity";
             Refresh();
-            Thread.Sleep(1000);
+            Thread.Sleep(pause);
+
             //display pencil price+quantity and pause
             receiptOutput.Text += $" \n\n Pencil         {pencilPrice.ToString("C")}   x {pencilQuantity.ToString("0")}";
             Refresh();
-            Thread.Sleep(1000);
+            Thread.Sleep(pause);
+
             //display stapler price+quantity and pause
             receiptOutput.Text += $" \n Stapler        {staplerPrice.ToString("C")}  x {staplerQuantity.ToString("0")}";
             Refresh();
-            Thread.Sleep(1000);
+            Thread.Sleep(pause);
+
             //display pencil case price+quantity and pause
             receiptOutput.Text += $" \n Pencil Case    {casePrice.ToString("C")}  x {pencilQuantity.ToString("0")}";
             Refresh();
-            Thread.Sleep(1000);
+            Thread.Sleep(pause);
 
+            // display subtotal and pause
+            receiptOutput.Text += $" \n\n\n Sub-Total        {subtotal.ToString("C")}";
+            Refresh();
+            Thread.Sleep(pause);
 
+            // display tax and pause
+            receiptOutput.Text += $"          Tax              {taxAmount.ToString("C")}";
+            Refresh();
+            Thread.Sleep(pause);
 
+            // display total
+            receiptOutput.Text += $"\n Total            {total.ToString("C")}";
+            Refresh();
+            Thread.Sleep(pause);
+
+            // display tendered
+            receiptOutput.Text += $"\n Tendered         {tendered.ToString("C")}";
+            Refresh();
+            Thread.Sleep(pause);
+
+            // display change 
+            receiptOutput.Text += $"\n\n Change           {change.ToString("C")}";
 
         }
 
-        private void item3Label_Click(object sender, EventArgs e)
+        private void newButton_Click(object sender, EventArgs e)
         {
-
+            // clear all inputs
+            subtotalOutput.Text = "";
+            taxOutput.Text = "";
+            totalOutput.Text = "";
+            changeOutput.Text = "";
+            receiptOutput.Text = "";
+            pencilInput.Text = "";
+            staplerInput.Text = "";
+            caseInput.Text = "";
+            tenderedInput.Text = "";
+            changeAmountOutput.Text = "";
+            changeLetterOutput.Text = "";
+            
         }
     }
 }
